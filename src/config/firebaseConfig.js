03f98970938c5c3af4ff6ea77204526085d13978
Zain Ul Mustafa <zain2984@gmail.com>
2024-03-28 21:08:13 -0300

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -6,13 +6,13 @@ import 'firebase/database'
 import functions from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwbjKSfD_rFJkcMGd8C-UnvK6xctKV4LQ",
-  authDomain: "sris-a8c0c.firebaseapp.com",
-  projectId: "sris-a8c0c",
-  storageBucket: "sris-a8c0c.appspot.com",
-  messagingSenderId: "569087864283",
-  appId: "1:569087864283:web:c15ff1e86ed23afe508bfe",
-  measurementId: "G-1YJ0E2L4SV"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
